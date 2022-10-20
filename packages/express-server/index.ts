6f3848db01e0bbdadec8ca870e7c85d11501451f
DelgadoArlee <delgadoarlee@gmail.com>
2022-10-20 15:55:42 +0800

    Refactored to eslint rules

--- packages/express-server/index.ts
+++ packages/express-server/index.ts
@@ -1,13 +1,14 @@
-import "dotenv/config";
-import http from "http";
+/* eslint-disable no-fallthrough */
+import 'dotenv/config';
+import http from 'http';
 import express, {
   Express,
   json,
   Request,
   Response,
   NextFunction,
-} from "express";
-import cors from "cors";
+} from 'express';
+import cors from 'cors';
 
 const app: Express = express();
 
@@ -16,7 +17,7 @@ const isNan = (obj: any) => {
 };
 
 const normalizePort = (val: string | number) => {
-  const port = typeof val == "string" ? parseInt(val, 10) : val;
+  const port = typeof val == 'string' ? parseInt(val, 10) : val;
 
   if (isNan(port)) {
     // named pipe
@@ -35,17 +36,15 @@ const port = normalizePort(process.env.PORT || 5000);
 const server = http.createServer(app);
 
 const onError = (error: any) => {
-  if (error.syscall !== "listen") {
+  if (error.syscall !== 'listen') {
     throw error;
   }
 
   switch (error.code) {
-    case "EACCES":
+    case 'EACCESS':
       process.exit(1);
-      break;
-    case "EADDRINUSE":
+    case 'EADDRINUSE':
       process.exit(1);
-      break;
     default:
       throw error;
   }
@@ -53,7 +52,7 @@ const onError = (error: any) => {
 
 const onListening = () => {
   const addr = server.address();
-  const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr?.port}`;
+  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr?.port}`;
   console.log(`Listening on ${bind}`);
 };
 
@@ -61,14 +60,14 @@ server.listen(port, () => {
   console.log(`Fisherfolk Record App listening at http://localhost:${port}`);
 });
 
-server.on("error", onError);
-server.on("listening", onListening);
+server.on('error', onError);
+server.on('listening', onListening);
 
 app.use(cors());
 app.use(json());
 
-app.get("/", (req: Request, res: Response, next: NextFunction) =>
-  res.send("HELLO HOOK NINJAS!!!")
+app.get('/', (req: Request, res: Response, next: NextFunction) =>
+  res.send('HELLO HOOK NINJAS!!!')
 );
 
 export default app;
