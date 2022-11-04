9ee95ee7dfa1e8ba7d2fbec308f04f32e1237eb4
karmslajo <karms.lajo@gmail.com>
2022-11-04 23:13:20 +0800

    Modify fisherfolk model and fisherfolk query

--- packages/express-server/src/graphql/schema/query/Fisherfolk/Fisherfolk.query.ts
+++ packages/express-server/src/graphql/schema/query/Fisherfolk/Fisherfolk.query.ts
@@ -4,9 +4,11 @@ import { list, nonNull, queryField } from 'nexus';
 const Fisherfolks = queryField('fisherfolks', {
   type: nonNull(list(nonNull('Fisherfolk'))),
   resolve: (_parent, _args, ctx) => ctx.prisma.fisherfolk.findMany({
-    where: {
-      registrationDate: 'desc',
-    }
+    orderBy: [
+      {
+        registrationDate: 'desc'
+      }
+    ]
   })
 });
 
