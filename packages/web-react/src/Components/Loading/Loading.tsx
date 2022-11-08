1968335f09b5c2b552925c549f9a6390fab6fd9d
DelgadoArlee <delgadoarlee@gmail.com>
2022-11-08 16:49:23 +0800

    Created Fisherfolk Table and Loading Component

---
+++ packages/web-react/src/Components/Loading/Loading.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Grid, CircularProgress } from '@mui/material';
+import { Box } from '@mui/system';
+
+function Loading() {
+  return (
+    <Box sx={{ padding: '25%' }}>
+      <Grid
+        container
+        sx={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          alignContent: 'center',
+        }}
+      >
+        <Grid item>
+          <CircularProgress size={100} />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
+
+export default Loading;
