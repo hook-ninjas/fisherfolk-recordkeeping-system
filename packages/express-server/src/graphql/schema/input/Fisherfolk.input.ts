817a0de1b8f471465629f190c74408bc0476dac8
DelgadoArlee <delgadoarlee@gmail.com>
2022-11-13 18:43:14 +0800

    Adapted schema changes to Input models

--- packages/express-server/src/graphql/schema/input/Fisherfolk.input.ts
+++ packages/express-server/src/graphql/schema/input/Fisherfolk.input.ts
@@ -4,19 +4,16 @@ import {
   EducationalBackground,
   FisherfolkStatus,
   Gender,
-  Nationality,
-  RegistrationType,
   Salutation,
-  SourceOfIncome,
 } from '../enums/';
 
 const CreateFisherfolkInput = inputObjectType({
   name: 'CreateFisherfolkInput',
   definition(t) {
-    t.nonNull.field('registrationType', { type: RegistrationType });
     t.nonNull.string('lastName');
     t.nonNull.string('firstName');
     t.nonNull.string('middleName');
+    t.nonNull.field('registrationDate', { type: 'DateTime' });
     t.nonNull.int('age');
     t.nonNull.field('salutation', { type: Salutation });
     t.nonNull.string('barangay');
@@ -29,25 +26,14 @@ const CreateFisherfolkInput = inputObjectType({
     t.nonNull.string('religion');
     t.nonNull.field('gender', { type: Gender });
     t.nonNull.field('civilStatus', { type: CivilStatus });
-    t.nullable.int('numOfChildren');
-    t.nonNull.field('nationality', { type: Nationality });
+    t.nonNull.int('numOfChildren');
+    t.nonNull.string('nationality');
     t.nonNull.field('educationalBackground', { type: EducationalBackground });
     t.nonNull.string('personToNotify');
     t.nonNull.string('ptnRelationship');
     t.nonNull.string('ptnAddress');
     t.nonNull.string('ptnContactNum');
-    t.nonNull.field('mainSrcOfIncome', { type: SourceOfIncome });
-    t.nullable.field('otherSrcOfIncome', { type: SourceOfIncome });
-    t.nullable.string('mainSrcGear');
-    t.nullable.string('otherSrcGear');
-    t.nullable.string('mainSrcMethod');
-    t.nullable.string('otherSrcMethod');
-    t.nullable.string('orgName');
-    t.nullable.int('orgYearMember');
-    t.nullable.string('orgPosition');
-    t.nonNull.string('image');
-    t.nonNull.string('signature');
-    t.nonNull.field('status', { type: FisherfolkStatus});
+    t.nonNull.field('status', { type: FisherfolkStatus });
   },
 });
 
