6e6fa6e99b05d8a1598237941c5647289817ac71
DelgadoArlee <delgadoarlee@gmail.com>
2022-11-13 18:39:58 +0800

    Adapted models and enums to schema changes

--- packages/express-server/src/graphql/schema/enums/index.ts
+++ packages/express-server/src/graphql/schema/enums/index.ts
@@ -1,10 +1,5 @@
 import { enumType } from 'nexus';
 
-export const RegistrationType = enumType({
-  name: 'RegistrationType',
-  members: ['RENEWAL', 'NEW_REGISTRATION'],
-});
-
 export const Salutation = enumType({
   name: 'Salutation',
   members: ['MR', 'MRS', 'MS'],
@@ -20,11 +15,6 @@ export const CivilStatus = enumType({
   members: ['SINGLE', 'MARRIED', 'LEGALLY_SEPARATED', 'WIDOWED'],
 });
 
-export const Nationality = enumType({
-  name: 'Nationality',
-  members: ['FILIPINO']
-});
-
 export const EducationalBackground = enumType({
   name: 'EducationalBackground',
   members: [
@@ -36,34 +26,15 @@ export const EducationalBackground = enumType({
   ],
 });
 
-export const SourceOfIncome = enumType({
-  name: 'SourceOfIncome',
-  members: [
-    'CAPTURE_FISHING',
-    'AQUACULTURE',
-    'FISH_VENDING',
-    'FISH_PROCESSING',
-  ],
-});
-
 export const FisherfolkStatus = enumType({
   name: 'FisherfolkStatus',
   members: ['ACTIVE', 'INACTIVE', 'DECEASED'],
-});
-
-export const PermitStatus = enumType({
-  name: 'PermitStatus',
-  members: ['PENDING', 'ON_RELEASE'],
 });
 
 export default [
   CivilStatus,
   EducationalBackground,
   FisherfolkStatus,
-  Nationality,
   Gender,
-  PermitStatus,
-  RegistrationType,
   Salutation,
-  SourceOfIncome,
 ];
