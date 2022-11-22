b3aa81aa252ed765efc1148df97788afb26d3de6
karmslajo <karms.lajo@gmail.com>
2022-11-23 01:03:59 +0800

    adjust bgcolor opacity and add text color

--- packages/web-react/src/Components/Buttons/CustomStatusButton.tsx
+++ packages/web-react/src/Components/Buttons/CustomStatusButton.tsx
@@ -6,10 +6,16 @@ interface FisherfolkStatusProps {
   label: FisherfolkStatus;
 }
 
-const fisherfolkStatusToColor = {
-  [FisherfolkStatus.Active]: 'rgba(83, 247, 137, .70)',
-  [FisherfolkStatus.Inactive]: 'rgba(228, 45, 45, .70)',
-  [FisherfolkStatus.Deceased]: 'rgba(122, 116, 116, .70)',
+const fisherfolkStatusColor = {
+  [FisherfolkStatus.Active]: 'rgba(83, 247, 137, .20)',
+  [FisherfolkStatus.Inactive]: 'rgba(228, 45, 45, .20)',
+  [FisherfolkStatus.Deceased]: 'rgba(122, 116, 116, .20)',
+};
+
+const textColor = {
+  [FisherfolkStatus.Active]: '#0EAC28',
+  [FisherfolkStatus.Inactive]: '#E91818',
+  [FisherfolkStatus.Deceased]: '#7D7777',
 };
 
 export const FisherfolkStatusButton = (status: FisherfolkStatusProps) => {
@@ -19,11 +25,12 @@ export const FisherfolkStatusButton = (status: FisherfolkStatusProps) => {
       justifyContent="center"
       sx={{
         width: '100%',
-        p: .05,
-        backgroundColor: fisherfolkStatusToColor[status.label],
+        p: 0.125,
+        backgroundColor: fisherfolkStatusColor[status.label],
         borderRadius: 2,
         fontSize: 12,
-        color: 'white'
+        fontWeight: 'medium',
+        color: textColor[status.label],
       }}
     >
       {status.label}
