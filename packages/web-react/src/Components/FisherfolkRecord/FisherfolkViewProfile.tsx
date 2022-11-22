2aa5d4e14c85c83165f095ee753395a1dde065cb
karmslajo <karms.lajo@gmail.com>
2022-11-22 16:48:55 +0800

    add table pagination on boats and gear view

--- packages/web-react/src/Components/FisherfolkRecord/FisherfolkViewProfile.tsx
+++ packages/web-react/src/Components/FisherfolkRecord/FisherfolkViewProfile.tsx
@@ -14,7 +14,12 @@ import {
   TableRow,
   Tab,
   Tabs,
+  TableBody,
+  Menu,
+  MenuItem,
+  TablePagination,
 } from '@mui/material';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -24,28 +29,115 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
-const vesselRows = [
-  'Id',
-  'Date',
-  'Homport',
-  'Name',
-  'Status',
-  'Type',
-  'Place Built',
-  'Year Built',
-  'Material',
-  'Length',
-  'Breadth',
-  'Depth',
-  'Ton Length',
-  'Ton Breadth',
-  'Ton Depth',
-  'Gross Ton',
-  'Net Ton',
-  'Engine Make',
-  'Horsepower',
-  'Serial Number',
+interface GearColumn {
+  id: 'id' | 'registrationDate' | 'classification' | 'name' | 'status';
+  label: string;
+  align?: 'right';
+}
+
+interface VesselColumn {
+  id:
+    | 'id'
+    | 'registrationDate'
+    | 'homeport'
+    | 'name'
+    | 'status'
+    | 'type'
+    | 'placeBuilt'
+    | 'yearBuilt'
+    | 'material'
+    | 'length'
+    | 'breadth'
+    | 'depth'
+    | 'tonLength'
+    | 'tonBreadth'
+    | 'tonDepth'
+    | 'grossTon'
+    | 'netTon'
+    | 'engineMake'
+    | 'serialNum'
+    | 'horsepower';
+  label: string;
+  align?: 'right';
+  minWidth?: number
+}
+
+const gearColumns: readonly GearColumn[] = [
+  { id: 'id', label: 'Id' },
+  { id: 'registrationDate', label: 'Date Registered', align: 'right' },
+  { id: 'classification', label: 'Classification', align: 'right' },
+  { id: 'name', label: 'Name', align: 'right' },
+  { id: 'status', label: 'Status', align: 'right' },
 ];
+
+const vesselColumns: readonly VesselColumn[] = [
+  { id: 'id', label: 'Id', },
+  { id: 'registrationDate', label: 'Date Registered', align: 'right' },
+  { id: 'homeport', label: 'Homeport', align: 'right' },
+  { id: 'name', label: 'Name', align: 'right' },
+  { id: 'status', label: 'Status', align: 'right' },
+  { id: 'type', label: 'Type', align: 'right' },
+  { id: 'placeBuilt', label: 'Place Built', align: 'right' },
+  { id: 'yearBuilt', label: 'Year Built', align: 'right' },
+  { id: 'material', label: 'Material', align: 'right' },
+  { id: 'length', label: 'Length', align: 'right' },
+  { id: 'breadth', label: 'Breadth', align: 'right' },
+  { id: 'depth', label: 'Depth', align: 'right' },
+  { id: 'tonLength', label: 'Ton Length', align: 'right' },
+  { id: 'tonBreadth', label: 'Ton Breadth', align: 'right' },
+  { id: 'tonDepth', label: 'Ton Depth', align: 'right' },
+  { id: 'grossTon', label: 'Gross Ton', align: 'right' },
+  { id: 'netTon', label: 'Net Ton', align: 'right' },
+  { id: 'engineMake', label: 'Engine Make', align: 'right' },
+  { id: 'serialNum', label: 'Serial Number', align: 'right' },
+  { id: 'horsepower', label: 'Horsepower', align: 'right' },
+];
+
+const vessels = {
+  data: [
+    {
+      id: '10001',
+      registrationDate: '2022-06-28T00:00:00.00',
+      homeport: 'Iloilo',
+      name: 'J&J',
+      status: 'Active',
+      type: 'Motorized',
+      placeBuilt: 'Iloilo',
+      yearBuilt: 2019,
+      material: 'Wood',
+      length: 3.4,
+      breadth: 2.3,
+      depth: 1.2,
+      tonLength: 3.4,
+      tonBreadth: 2.3,
+      tonDepth: 1.2,
+      grossTon: 4,
+      netTon: 2,
+      engineMake: 'SDGDSGD',
+      serialNum: 'JXSAJXNAJ09',
+      horsepower: 2000,
+    },
+  ],
+};
+
+const gears = {
+  data: [
+    {
+      id: '10001',
+      registrationDate: '2022-06-28T00:00:00.00',
+      classification: 'Hook and Line',
+      name: 'Simple',
+      status: 'Active',
+    },
+    {
+      id: '10001',
+      registrationDate: '2022-06-28T00:00:00.00',
+      classification: 'Hook and Line',
+      name: 'Simple',
+      status: 'Active',
+    },
+  ],
+};
 
 interface InfoIProps {
   title: string;
@@ -113,35 +205,195 @@ function a11yProps(index: number) {
 }
 
 function GearTable() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event: unknown, newPage: number) =>
+    setPage(newPage);
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+  const [drop, setDropDown] = React.useState<null | HTMLElement>(null);
+  const handleDismissDropdown = () => setDropDown(null);
+  const open = Boolean(drop);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) =>
+    setDropDown(event.currentTarget);
+
   return (
-    <TableContainer component={Paper} sx={{ m: -2.5, marginLeft: -3, width: '106%' }}>
-      <Table size="small" aria-label="gear-table">
+    <TableContainer
+      component={Paper}
+      sx={{ m: -2.5, marginLeft: -3, width: '106%' }}
+    >
+      <Table stickyHeader size="small" aria-label="gear-table">
         <TableHead>
           <TableRow>
-            <TableCell>Id</TableCell>
-            <TableCell align="right">Date Registered</TableCell>
-            <TableCell align="right">Classification</TableCell>
-            <TableCell align="right">Name</TableCell>
-            <TableCell align="right">Status</TableCell>
+            {gearColumns.map((gear) => (
+              <TableCell key={gear.id} align={gear.align}>
+                {gear.label}
+              </TableCell>
+            ))}
           </TableRow>
         </TableHead>
+        <TableBody>
+          {gears &&
+            gears.data
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((gear) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={gear.id}>
+                    {gearColumns.map((column) => {
+                      const value = gear[column.id];
+                      const formattedDate = new Date(
+                        gear[column.id]
+                      ).toLocaleDateString();
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.id === 'registrationDate'
+                            ? formattedDate
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                    <TableCell align="right">
+                      <Button
+                        id="basic-button"
+                        aria-controls={open ? 'basic-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        onClick={(e) => {
+                          handleClick(e);
+                        }}
+                        style={{ color: '#808080' }}
+                      >
+                        <MoreVertIcon />
+                      </Button>{' '}
+                      <Menu
+                        id="dropdwown-menu"
+                        anchorEl={drop}
+                        open={open}
+                        onClose={handleDismissDropdown}
+                        MenuListProps={{
+                          'aria-labelledby': 'basic-button',
+                        }}
+                      >
+                        <MenuItem>Edit</MenuItem>
+                        <MenuItem>Archive</MenuItem>
+                      </Menu>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+        </TableBody>
       </Table>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25, 50]}
+        component="div"
+        count={gears.data.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
     </TableContainer>
   );
 }
 
 function VesselTable() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event: unknown, newPage: number) =>
+    setPage(newPage);
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+  const [drop, setDropDown] = React.useState<null | HTMLElement>(null);
+  const handleDismissDropdown = () => setDropDown(null);
+  const open = Boolean(drop);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) =>
+    setDropDown(event.currentTarget);
+
   return (
-    <TableContainer component={Paper} sx={{ m: -2.5, marginLeft: -3, width: '106%' }}>
-      <Table size="medium" aria-label="vessel-table">
+    <TableContainer
+      component={Paper}
+      sx={{ m: -2.5, marginLeft: -3, width: '106%' }}
+    >
+      <Table stickyHeader size="small" aria-label="vessel-table">
         <TableHead>
           <TableRow>
-            {vesselRows.map((row) => {
-              return <TableCell key={row}>{row}</TableCell>;
-            })}
+            {vesselColumns.map((vessel) => (
+              <TableCell key={vessel.id} align={vessel.align} sx={{minWidth: 100}}>
+                {vessel.label}
+              </TableCell>
+            ))}
           </TableRow>
         </TableHead>
       </Table>
+      <TableBody>
+        {vessels &&
+          vessels.data
+            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            .map((vessel) => {
+              return (
+                <TableRow hover role="checkbox" tabIndex={-1} key={vessel.id}>
+                  {vesselColumns.map((column) => {
+                    const value = vessel[column.id];
+                    const formattedDate = new Date(
+                      vessel[column.id]
+                    ).toLocaleDateString();
+                    return (
+                      <TableCell key={column.id} align={column.align} sx={{minWidth: 100}}>
+                        {column.id === 'registrationDate'
+                          ? formattedDate
+                          : value}
+                      </TableCell>
+                    );
+                  })}
+                  <TableCell align="right">
+                    <Button
+                      id="basic-button"
+                      aria-controls={open ? 'basic-menu' : undefined}
+                      aria-haspopup="true"
+                      aria-expanded={open ? 'true' : undefined}
+                      onClick={(e) => {
+                        handleClick(e);
+                      }}
+                      style={{ color: '#808080' }}
+                    >
+                      <MoreVertIcon />
+                    </Button>{' '}
+                    <Menu
+                      id="dropdwown-menu"
+                      anchorEl={drop}
+                      open={open}
+                      onClose={handleDismissDropdown}
+                      MenuListProps={{
+                        'aria-labelledby': 'basic-button',
+                      }}
+                    >
+                      <MenuItem>Edit</MenuItem>
+                      <MenuItem>Archive</MenuItem>
+                    </Menu>
+                  </TableCell>
+                </TableRow>
+              );
+            })}
+      </TableBody>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25, 50]}
+        component="div"
+        count={vessels.data.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
     </TableContainer>
   );
 }
@@ -155,7 +407,11 @@ function BasicTabs() {
 
   return (
     <Box sx={{ width: '100%' }}>
-      <Grid container justifyContent='space-between' sx={{ borderBottom: 1, borderColor: 'divider' }}>
+      <Grid
+        container
+        justifyContent="space-between"
+        sx={{ borderBottom: 1, borderColor: 'divider' }}
+      >
         <Tabs
           value={value}
           onChange={handleChange}
