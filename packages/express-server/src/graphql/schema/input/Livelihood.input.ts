d179513fff11cadeb700b092a26ec5b403d85a6d
DelgadoArlee <delgadoarlee@gmail.com>
2022-11-24 21:16:58 +0800

    Removed isArchive field

--- packages/express-server/src/graphql/schema/input/Livelihood.input.ts
+++ packages/express-server/src/graphql/schema/input/Livelihood.input.ts
@@ -7,7 +7,6 @@ const LivelihoodInput = inputObjectType({
     t.field('type', { type: SourceOfIncome });
     t.string('description');
     t.boolean('isMain');
-    t.boolean('isArchive');
   },
 });
 
