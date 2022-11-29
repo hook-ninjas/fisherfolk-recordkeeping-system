3160ddefa594c4cbf35f37c0e637836e69005368
karmslajo <karms.lajo@gmail.com>
2022-11-30 00:23:06 +0800

    add fisherfolk gear and boat record

---
+++ packages/web-react/src/Components/FisherfolkRecord/FisherfolkGearRecord.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Box, Grid, Paper, Typography } from '@mui/material';
+import FisherfolkGearTable from '../Table/FisherfolkGearTable';
+
+const FisherfolkGearRecord = () => {
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Paper
+          sx={{
+            p: 2,
+            display: 'flex',
+            flexDirection: 'column',
+            boxShadow: 2,
+          }}
+        >
+          <Box m={1}>
+            <Typography variant="h5">Fisherfolk Gear Record</Typography>
+          </Box>
+          <Grid item>
+            <Box m={1}>
+              <FisherfolkGearTable />
+            </Box>
+          </Grid>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default FisherfolkGearRecord;
