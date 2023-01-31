6760f36c833f89d5796220e0c7ad0d19b985397a
karmslajo <karms.lajo@gmail.com>
2023-01-31 18:06:25 +0800

    update create user test and add login user tests

--- packages/express-server/src/graphql/schema/mutation/User/User.test.ts
+++ packages/express-server/src/graphql/schema/mutation/User/User.test.ts
@@ -1,7 +1,8 @@
 import { User } from '@prisma/client';
 import { Context, createMockContext } from '../../../context';
-import { createUser } from './User.resolver';
+import { createUser, loginUser } from './User.resolver';
 import { MockContext } from '../../../../types/types';
+import bcrypt from 'bcrypt';
 
 let mockCtx: MockContext;
 let ctx: Context;
@@ -11,26 +12,89 @@ beforeEach(() => {
   ctx = mockCtx as unknown as Context;
 });
 
-test('should create new user', async () => {
-  const user: User = {
-    id: 1,
-    username: 'user_admin',
-    password: 'password123'
-  };
-
-  mockCtx.prisma.user.create.mockResolvedValue(user);
-
-  const input = {
-    username: 'user_admin',
-    password: 'password123'
-  };
-
-  await expect(createUser(input, ctx)).resolves.toEqual(user);
-
-  expect(ctx.prisma.user.create).toHaveBeenCalledWith({
-    data: {
-      username: 'user_admin',
-      password: 'password123'
-    }
+describe('Create Account', () => {
+  it('should create new user', async () => {
+    const user: User = {
+      id: 1,
+      username: 'admin_jc',
+      password: 'Admin_2023',
+    };
+  
+    mockCtx.prisma.user.create.mockResolvedValue(user);
+  
+    const input = {
+      username: 'admin_jc',
+      password: 'Admin_2023',
+    };
+  
+    const payload = createUser(input, ctx);
+  
+    await expect(payload).resolves.toEqual({
+      token: (await payload).token,
+      user: (await payload).user,
+    });
   });
+});
+
+describe('Login', () => {
+  it('should log in user', async () => {
+    const user: User = {
+      id: 1,
+      username: 'admin_jc',
+      password: await bcrypt.hash('Admin_2023', 10),
+    };
+  
+    mockCtx.prisma.user.findUnique.mockResolvedValue(user);
+  
+    const input = {
+      username: 'admin_jc',
+      password: 'Admin_2023',
+    };
+  
+    const loginUserPayload = loginUser(input, ctx);
+  
+    await expect(loginUserPayload).resolves.toEqual({
+      token:  (await loginUserPayload).token,
+      user: (await loginUserPayload).user
+    });
+  });
+
+  it('should throw can\'t find account error', async () => {
+    const user: User = {
+      id: 1,
+      username: 'admin_jc',
+      password: 'Admin_2023',
+    };
+  
+    mockCtx.prisma.user.create.mockResolvedValue(user);
+  
+    const input = {
+      username: 'admin_lj',
+      password: 'Admin_2023',
+    };
+  
+    const loginUserPayload = loginUser(input, ctx);
+
+    await expect(loginUserPayload).rejects.toThrowError('Sorry, could not find your account.');
+  });
+
+  it('should throw incorrect password error', async () => {
+    const user: User = {
+      id: 1,
+      username: 'admin_jc',
+      password: await bcrypt.hash('Admin_2023', 10),
+    };
+  
+    mockCtx.prisma.user.findUnique.mockResolvedValue(user);
+  
+    const input = {
+      username: 'admin_jc',
+      password: 'Admin_2022',
+    };
+  
+    const loginUserPayload = loginUser(input, ctx);
+
+    await expect(loginUserPayload).rejects.toThrowError('Incorrect password');
+  });
+
 });
