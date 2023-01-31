be19b54791675906673c8deaeccbdc6b80d8e6b9
karmslajo <karms.lajo@gmail.com>
2023-01-31 19:28:19 +0800

    remove unused lib

--- packages/web-react/src/Tests/Auth/Logout.test.tsx
+++ packages/web-react/src/Tests/Auth/Logout.test.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { BrowserRouter } from 'react-router-dom';
-import Sidebar from '../../Components/SideBar/SideBar';
 import CustomizedDialogs from '../../Components/ConfirmationDialog/ConfirmationDialog';
 import { vi } from 'vitest';
 
