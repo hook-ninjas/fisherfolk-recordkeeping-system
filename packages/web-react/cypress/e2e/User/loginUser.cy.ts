1b95ea7580ad60bce1205279c565a4b3d13ea4bd
karmslajo <karms.lajo@gmail.com>
2023-02-02 21:24:15 +0800

    e2e tests on user create account and login

---
+++ packages/web-react/cypress/e2e/User/loginUser.cy.ts
@@ -0,0 +1,52 @@
+describe('Login User', () => {
+  const username = 'admin_rio';
+  const password = 'city_Agri2023';
+
+  before(() => {
+    cy.visit('/create-account');
+  });
+
+  it('should create new user first', () => {
+    cy.get('[name=username]')
+      .click()
+      .type(username)
+      .should('have.value', username)
+      .get('[name=password]')
+      .click()
+      .type(password)
+      .should('have.value', password);
+  });
+
+  it('should successfuly save new user', () => {
+    cy.get('[type=submit]')
+      .should('contain', 'Create account')
+      .click()
+      .get('.MuiSnackbar-root')
+      .should('be.visible');
+  });
+
+  it('should direct to login page', () => {
+    cy.url().should('eq', 'http://localhost:5173/login');
+  });
+
+  it('should input username and password', () => {
+    cy.get('[name=username]')
+      .click()
+      .type(username)
+      .should('have.value', username)
+      .get('[name=password]')
+      .click()
+      .type(password)
+      .should('have.value', password);
+  });
+
+  it('should successfuly login user', () => {
+    cy.get('[type=submit]')
+      .should('contain', 'Continue')
+      .click();
+  });
+
+  it('should direct to dashboard page', () => {
+    cy.url().should('eq', 'http://localhost:5173/dashboard');
+  });
+});
