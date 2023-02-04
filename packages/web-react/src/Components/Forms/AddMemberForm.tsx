1c8ecdba79ed545f02393d4a4ec2d520dae10d26
DelgadoArlee <delgadoarlee@gmail.com>
2023-02-04 10:59:06 +0800

    Moved Validation schema to dedicated schema folder

--- packages/web-react/src/Components/Forms/AddMemberForm.tsx
+++ packages/web-react/src/Components/Forms/AddMemberForm.tsx
@@ -22,7 +22,7 @@ import {
   FormInputAutoText,
 } from './FormInputFields';
 import { useForm } from 'react-hook-form';
-import { object, string, mixed, number } from 'yup';
+
 import { yupResolver } from '@hookform/resolvers/yup';
 import {
   CreateFisherfolkDocument,
@@ -43,8 +43,8 @@ import {
   cityMunicipalityOptions,
   provinceOptions,
 } from './Enums';
-import { getValues } from '../../utils/utils';
 import PhotoUpload from '../Input/PhotoUpload';
+import { FfolkValidation } from './validation/schema';
 
 export interface FormContainerTitleProps {
   children?: React.ReactNode;
@@ -161,73 +161,13 @@ export default function AddFisherfolkForm({
   const { CaptureFishing, Aquaculture, FishVending, FishProcessing } =
     otherFishingActivities;
 
-  const addFisherfolkSchema = object().shape({
-    registrationType: string()
-      .nullable()
-      .oneOf(getValues(registrationTypes))
-      .required('Select registration type.'),
-    lastName: string().required('Enter last name.'),
-    firstName: string().required('Enter first name.'),
-    middleName: string().required('Enter middle name.'),
-    salutation: string()
-      .nullable()
-      .oneOf(getValues(salutationOptions))
-      .required('Select salutation.'),
-    contactNumber: string()
-      .required('Enter contact number.')
-      .matches(/^(09|\+639)\d{9}$/, 'Please enter a valid contact number.'),
-    barangay: string().required('Enter or select barangay.'),
-    cityMunicipality: string().required('Enter city/municipality.'),
-    province: string().required('Enter province.'),
-    residentYear: string().matches(/^\d{4}$/, 'Enter year of residency.'),
-    gender: string()
-      .nullable()
-      .oneOf(getValues(genderOptions))
-      .required('Select gender.'),
-    age: string()
-      .matches(/^$|\d{1,3}$/, 'Age must be a number.')
-      .required('Enter age.'),
-    dateOfBirth: string().nullable().required('Enter date of birth.'),
-    placeOfBirth: string().required('Enter place of birth.'),
-    civilStatus: string().required('Select civil status.'),
-    educationalBackground: mixed()
-      .nullable()
-      .oneOf(getValues(educationalBackgrounds))
-      .required('Enter or select educational background.'),
-    numOfChildren: string().matches(/^$|\d{1,2}$/, 'Enter a number.'),
-    nationality: mixed()
-      .nullable()
-      .oneOf(getValues(nationalities))
-      .required('Enter or select nationality.'),
-    personToNotify: string().required('Enter person to notify.'),
-    ptnRelationship: string().required(
-      'Enter relationship with person to notify.'
-    ),
-    ptnContactNum: string()
-      .required('Enter contact number of person to notify.')
-      .matches(/^(09|\+639)\d{9}$/, 'Please enter a valid contact number.'),
-    ptnAddress: string().required('Enter address of person to notify.'),
-    mainFishingActivity: string().required('Select main fishing activity.'),
-    orgMemberSince: string().matches(/^$|\d{4}$/, 'Please enter year.'),
-    profilePhoto: object()
-      .shape({
-        name: string().required(),
-        size: number().max(1000000, 'File over 1 mb'),
-        type: string()
-          .matches(/^.*(image\/jpeg|jpg|png)$/gm, 'File format not supported')
-          .required('No File Uploaded'),
-      })
-      .nullable()
-      .required('Add Profile Image'),
-  });
-
   const {
     register,
     control,
     handleSubmit,
     formState: { errors },
   } = useForm({
-    resolver: yupResolver(addFisherfolkSchema),
+    resolver: yupResolver(FfolkValidation),
   });
 
   const [createFisherfolk] = useMutation(CreateFisherfolkDocument, {
