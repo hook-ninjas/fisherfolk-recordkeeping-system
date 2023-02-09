bc708d0878a1e0c60043c3763fe9b35c6e437cef
karmslajo <karms.lajo@gmail.com>
2023-02-09 23:33:30 +0800

    add login and create account schema

--- packages/web-react/src/Components/Forms/validation/schema.ts
+++ packages/web-react/src/Components/Forms/validation/schema.ts
@@ -1,18 +1,18 @@
 import { object, string, mixed, number } from 'yup';
 import { getValues } from '../../../utils/utils';
 import {
-  registrationTypes,
   salutationOptions,
   genderOptions,
   educationalBackgroundOptions,
   nationalityOptions,
+  materialOptions,
 } from '../Enums';
 
-const FfolkValidation = object().shape({
-  registrationType: string()
-    .nullable()
-    .oneOf(getValues(registrationTypes))
-    .required('Select registration type.'),
+const FisherfolkSchema = object().shape({
+  // registrationType: string()
+  //   .nullable()
+  //   .oneOf(getValues(registrationTypes))
+    // .required('Select registration type.'),
   lastName: string().required('Enter last name.'),
   firstName: string().required('Enter first name.'),
   middleName: string().required('Enter middle name.'),
@@ -55,7 +55,9 @@ const FfolkValidation = object().shape({
     .matches(/^(09|\+639)\d{9}$/, 'Please enter a valid contact number.'),
   ptnAddress: string().required('Enter address of person to notify.'),
   mainFishingActivity: string().required('Select main fishing activity.'),
+  orgName: string(),
   orgMemberSince: string().matches(/^$|\d{4}$/, 'Please enter year.'),
+  orgPosition: string(),
   profilePhoto: object()
     .shape({
       name: string().required(),
@@ -65,7 +67,56 @@ const FfolkValidation = object().shape({
         .required('No File Uploaded'),
     })
     .nullable()
-    .required('Add Profile Image'),
+    // .required('Add Profile Image'),
 });
 
-export { FfolkValidation };
+const VesselWithGearSchema = object().shape({
+  vessel: object().shape({
+    engineMake: string(),
+    grossTonnage: string().matches(/^[0-9]\d*(\.\d+)?$/, 'Enter a number.'),
+    homeport: string(),
+    horsepower: string(),
+    mfvrNumber: string(),
+    material: string().nullable().oneOf(materialOptions),
+    name: string(),
+    netTonnage: string().matches(/^[0-9]\d*(\.\d+)?$/, 'Enter a number.'),
+    placeBuilt: string(),
+    registeredBreadth: string().matches(
+      /^[0-9]\d*(\.\d+)?$/,
+      'Enter a number.'
+    ),
+    registeredDepth: string().matches(
+      /^[0-9]\d*(\.\d+)?$/,
+      'Enter a number.'
+    ),
+    registeredLength: string().matches(
+      /^[0-9]\d*(\.\d+)?$/,
+      'Enter a number.'
+    ),
+    serialNumber: string(),
+    tonnageBreadth: string().matches(/^[0-9]\d*(\.\d+)?$/, 'Enter a number.'),
+    tonnageDepth: string().matches(/^[0-9]\d*(\.\d+)?$/, 'Enter a number.'),
+    tonnageLength: string().matches(/^[0-9]\d*(\.\d+)?$/, 'Enter a number.'),
+    type: string(),
+    yearBuilt: string().matches(/^$|\d{4}$/, 'Enter year.'),
+  }),
+});
+
+const CreateAccountSchema = object().shape({
+  username: string()
+    .required('Enter username.')
+    .min(6, 'Username must be atleast 6 characters.'),
+  password: string()
+    .required('Enter password.')
+    .matches(
+      /^(?=.*[A-Z])(?=.*[a-z])(?=.*?[0-9])(?=.*[#?!@$%^&*_-]).{8,}$/,
+      'Password must contain atleast 8 characters, one uppercase, one lowercase, one number and one special character.'
+    ),
+});
+
+const LoginSchema = object().shape({
+  username: string().required('Enter username.'),
+  password: string().required('Enter password.'),
+});
+
+export { FisherfolkSchema, VesselWithGearSchema, CreateAccountSchema, LoginSchema };
