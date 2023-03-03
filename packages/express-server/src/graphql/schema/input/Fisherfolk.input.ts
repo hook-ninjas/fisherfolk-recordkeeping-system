1692755a84a321d459af9cc5678761ce28ad949d
DelgadoArlee <delgadoarlee@gmail.com>
2023-03-03 19:37:19 +0800

    Updated salutation, residentYear, numOfChildren, and org fields to nullable

--- packages/express-server/src/graphql/schema/input/Fisherfolk.input.ts
+++ packages/express-server/src/graphql/schema/input/Fisherfolk.input.ts
@@ -16,18 +16,18 @@ const CreateFisherfolkInput = inputObjectType({
     t.string('middleName');
     t.string('appellation');
     t.int('age');
-    t.field('salutation', { type: Salutation });
+    t.nullable.field('salutation', { type: Salutation });
     t.string('barangay');
     t.string('cityMunicipality');
     t.string('province');
     t.string('contactNum');
-    t.int('residentYear');
+    t.nullable.int('residentYear');
     t.field('dateOfBirth', { type: 'DateTime' });
     t.string('placeOfBirth');
     t.string('religion');
     t.field('gender', { type: Gender });
     t.field('civilStatus', { type: CivilStatus });
-    t.int('numOfChildren');
+    t.nullable.int('numOfChildren');
     t.string('nationality');
     t.field('educationalBackground', { type: EducationalBackground });
     t.string('personToNotify');
@@ -35,8 +35,7 @@ const CreateFisherfolkInput = inputObjectType({
     t.string('ptnAddress');
     t.string('ptnContactNum');
     t.field('livelihoods', { type: list(LivelihoodInput) });
-    t.field('organization', { type: nullable(OrganizationInput) });
-
+    t.nullable.field('organization', { type: nullable(OrganizationInput) });
   },
 });
 
