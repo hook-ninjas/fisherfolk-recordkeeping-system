f0f5d341a1d00c6f599afc7fe95c6057ebc8939a
DelgadoArlee <delgadoarlee@gmail.com>
2023-03-19 15:55:30 +0800

    Removed nonNull function

--- packages/express-server/src/graphql/schema/mutation/Image/Image.mutation.ts
+++ packages/express-server/src/graphql/schema/mutation/Image/Image.mutation.ts
@@ -1,12 +1,11 @@
 import { mutationField } from 'nexus';
-import { nonNullArg } from '../../../../utils/utils';
-import ImageInput from '../../input/Image.input';
+import { ImageInput } from '../../input/Image.input';
 import { uploadImage } from './Image.resolver';
 
 const CreateImage = mutationField('createImage', {
   type: 'Image',
   args: {
-    data: nonNullArg(ImageInput),
+    data: ImageInput,
   },
   resolve: (_, args, context) => {
     const { data } = args;
