16b82debcee988a51816b8bdc582974059cc1490
DelgadoArlee <delgadoarlee@gmail.com>
2023-03-19 15:44:00 +0800

    Created new Vessel input for Ffolk form to graphql schema

--- packages/express-server/src/graphql/schema/input/Vessel.input.ts
+++ packages/express-server/src/graphql/schema/input/Vessel.input.ts
@@ -1,17 +1,44 @@
-import { inputObjectType } from 'nexus';
+import { inputObjectType, list } from 'nexus';
+import { UploadImageInput } from './Image.input';
 import { Material } from '../enums';
 
-const CreateVesselInput = inputObjectType({
-  name: 'CreateVesselInput',
+// Vessel Input for Registering Ffolk //
+const CreateFfolkVesselInput = inputObjectType({
+  name: 'CreateFfolkVesselInput',
   definition(t) {
-    t.field('fisherfolkId', {type: 'BigInt'});
     t.string('mfvrNumber');
     t.string('homeport');
     t.string('name');
     t.string('type');
     t.string('placeBuilt');
     t.nullable.int('yearBuilt');
-    t.nullable.field('material', {type: Material});
+    t.nullable.field('material', { type: Material });
+    t.nullable.float('registeredLength');
+    t.nullable.float('registeredBreadth');
+    t.nullable.float('registeredDepth');
+    t.nullable.float('tonnageLength');
+    t.nullable.float('tonnageBreadth');
+    t.nullable.float('tonnageDepth');
+    t.nullable.float('grossTonnage');
+    t.nullable.float('netTonnage');
+    t.string('engineMake');
+    t.string('serialNumber');
+    t.nullable.float('horsepower');
+    t.field('files', { type: list(UploadImageInput) });
+  },
+});
+
+const CreateVesselInput = inputObjectType({
+  name: 'CreateVesselInput',
+  definition(t) {
+    t.field('fisherfolkId', { type: 'BigInt' });
+    t.string('mfvrNumber');
+    t.string('homeport');
+    t.string('name');
+    t.string('type');
+    t.string('placeBuilt');
+    t.nullable.int('yearBuilt');
+    t.nullable.field('material', { type: Material });
     t.nullable.float('registeredLength');
     t.nullable.float('registeredBreadth');
     t.nullable.float('registeredDepth');
@@ -26,4 +53,4 @@ const CreateVesselInput = inputObjectType({
   },
 });
 
-export default CreateVesselInput;
+export { CreateVesselInput, CreateFfolkVesselInput };
