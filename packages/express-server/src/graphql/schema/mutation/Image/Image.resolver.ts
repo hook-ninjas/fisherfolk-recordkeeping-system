4ec84a01e62dfeb5f28e0b4c69cb570d7985852a
DelgadoArlee <delgadoarlee@gmail.com>
2023-03-19 15:56:40 +0800

    Added new image resolvers

--- packages/express-server/src/graphql/schema/mutation/Image/Image.resolver.ts
+++ packages/express-server/src/graphql/schema/mutation/Image/Image.resolver.ts
@@ -1,23 +1,38 @@
-import { context, Context } from '../../../context';
+import { Context } from '../../../context';
 import { NexusGenInputs } from '../../../generated/nexus';
 import cloudinary from 'cloudinary';
-import 'dotenv/config';
-
 
 type CreateImageInput = NexusGenInputs['CreateImageInput'];
+type UploadImageInput = NexusGenInputs['UploadImageInput'];
 
-export async function uploadImage(
-  image: CreateImageInput,
-  ctx: Context) {
-  
-  cloudinary.v2.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+const cloudURL = async (imageURI: string) => {
+  const result = await cloudinary.v2.uploader.upload(imageURI, {
+    folder: 'fisherfolk-recordkeeping-system',
   });
-  
 
-  const result = await cloudinary.v2.uploader.upload(image.url, {folder: 'fisherfolk-recordkeeping-system'});
+  return result.url;
+};
+
+const createImage = async (uploadImageInput: UploadImageInput) => {
+  const { name, size, type, uri, isProfileImage } = uploadImageInput;
+  const cloudinaryURL = await cloudURL(uri);
+
+  return {
+    name: name,
+    size: size,
+    type: type,
+    url: cloudinaryURL,
+    isProfileImage: isProfileImage,
+  };
+};
+
+const createImages = async (files: UploadImageInput[]) =>
+  await Promise.all(files.map(createImage));
+
+async function uploadImage(image: CreateImageInput, ctx: Context) {
+  const result = await cloudinary.v2.uploader.upload(image.url, {
+    folder: 'fisherfolk-recordkeeping-system',
+  });
   console.log(result.url); //checks cloudinary url
 
   return await ctx.prisma.image.create({
@@ -28,6 +43,8 @@ export async function uploadImage(
       name: result.signature,
       url: result.url,
       updatedAt: result.created_at,
-    }
-  });   
+    },
+  });
 }
+
+export { createImage, createImages, uploadImage };
