fc82d12cfdc7dc3740cc6ab26999890f179047d6
karmslajo <karms.lajo@gmail.com>
2023-03-25 00:10:32 +0800

    add imag on fisherfolk program card

--- packages/web-react/src/Components/Card/FisherfolkProgramCard.tsx
+++ packages/web-react/src/Components/Card/FisherfolkProgramCard.tsx
@@ -1,29 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Button,
   Card,
   CardActions,
   CardContent,
+  CardMedia,
   Typography,
 } from '@mui/material';
 import Divider from '@mui/material/Divider/Divider';
 import { Stack } from '@mui/system';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
 import DateRangeIcon from '@mui/icons-material/DateRange';
+import ViewFisherfolkProgram from '../FisherfolkProgram/ViewProgram';
+import { useQuery } from '@apollo/client';
+import { GovernmentAidDocument } from '../../graphql/generated';
 
 interface Props {
+  id: number;
   title: string;
   slot: number;
   date: Date;
 }
 
 export default function FisherfolkProgramCard({
+  id,
   title,
   slot,
   date,
 }: Props) {
+  const [viewProgramBtn, setViewProgramBtn] = useState(false);
+  const handleViewProgramOpen = () => setViewProgramBtn(true);
+  const handleViewProgramClose = () => setViewProgramBtn(false);
+
+  const { loading, data } = useQuery(GovernmentAidDocument, {
+    variables: {
+      govtAidId: id,
+    },
+  });
+
+  if (loading || !data) {
+    return null;
+  }
+
   return (
     <Card sx={{ borderRadius: 0 }}>
+      <CardMedia
+        sx={{ height: 90 }}
+        image={data && data.governmentAidPhotos[0]?.url}
+      />
       <CardContent>
         <Typography
           id="program-title"
@@ -35,7 +59,7 @@ export default function FisherfolkProgramCard({
           {title}
         </Typography>
         <Divider />
-        <Stack direction="row" justifyContent="space-between" mt={2} mb={-1.5}>
+        <Stack direction="row" justifyContent="space-between" mt={1} mb={-2}>
           <PeopleAltIcon sx={{ color: 'gray' }} />
           <Typography
             id="program-slot"
@@ -57,8 +81,28 @@ export default function FisherfolkProgramCard({
         </Stack>
       </CardContent>
       <CardActions>
-        <Button fullWidth size="small" variant="text">
-          View Program
+        <Button
+          fullWidth
+          size="small"
+          variant="text"
+          onClick={handleViewProgramOpen}
+        >
+          View
+        </Button>
+        {viewProgramBtn && (
+          <ViewFisherfolkProgram
+            govtAidId={id}
+            open={viewProgramBtn}
+            handleClose={handleViewProgramClose}
+          />
+        )}
+        <Button
+          fullWidth
+          size="small"
+          variant="text"
+          // onClick={handleViewProgramOpen}
+        >
+          Edit
         </Button>
       </CardActions>
     </Card>
