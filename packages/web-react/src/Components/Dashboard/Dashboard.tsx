664cc63eacacc039b524aa185cbf8c65d878811f
karmslajo <karms.lajo@gmail.com>
2023-03-25 00:22:01 +0800

    remove white background display on dashboard page

--- packages/web-react/src/Components/Dashboard/Dashboard.tsx
+++ packages/web-react/src/Components/Dashboard/Dashboard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Grid, Paper, Typography } from '@mui/material';
+import { Box, Grid, Typography } from '@mui/material';
 import Livelihood from './Livelihood';
 import FisherfolkInfo from './Fisherfolk';
 import Fisherfolk from './CountFisherfolk';
@@ -8,22 +8,12 @@ function Dashboard() {
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
-        <Paper
-          sx={{
-            p: 0,
-            display: 'flex',
-            flexDirection: 'column',
-            boxShadow: 2,
-            minHeight: 700
-          }}
-        >
-          <Box m={1}>
-            <Typography variant="h5">Dashboard</Typography>
-          </Box>
-          <Fisherfolk/>
-          <FisherfolkInfo/>
-          <Livelihood />
-        </Paper>
+        <Box m={1}>
+          <Typography variant="h4">Dashboard</Typography>
+        </Box>
+        <Fisherfolk />
+        <FisherfolkInfo />
+        <Livelihood />
       </Grid>
     </Grid>
   );
