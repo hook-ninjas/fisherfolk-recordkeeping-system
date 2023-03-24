685579d76e0b8a720a7da48d1077f186de0c240d
karmslajo <karms.lajo@gmail.com>
2023-03-25 00:49:29 +0800

    remove duplicate test

--- packages/web-react/cypress/e2e/logout.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('Logout', () => {
-  before(() => {
-    cy.visit('/');
-  });
-
-  it('should logout user', () => {
-    cy.contains('Logout').click().get('[aria-label="Logout"]').click();
-  }); // should direct to login page
-});
