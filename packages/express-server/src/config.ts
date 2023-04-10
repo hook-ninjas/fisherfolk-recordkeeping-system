6413dcfeb5ca7d3c459acd43a9301955454a18cb
RenzoLaporno <renzogabriel.laporno-19@cpu.edu.ph>
2023-04-10 17:15:01 +0800

    change config typo

--- packages/express-server/src/config.ts
+++ packages/express-server/src/config.ts
@@ -1,6 +1,6 @@
 const url =
   process.env.VITE_PROD === 'true'
     ? process.env.VITE_SERVER_URL
-    : 'http://localhost:10000/graphql'
+    : 'http://localhost:4000/graphql';
 
 module.exports = url;
