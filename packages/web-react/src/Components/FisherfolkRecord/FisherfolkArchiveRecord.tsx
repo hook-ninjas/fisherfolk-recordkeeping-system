b4a702757ad3f2850d8095bf23d7f52a7b7c6437
Jenny Rose Suelan <jennyrose.suelan-20@cpu.edu.ph>
2023-04-26 23:33:14 +0800

    Modify archive record to include panel for gears and boats archive records

--- packages/web-react/src/Components/FisherfolkRecord/FisherfolkArchiveRecord.tsx
+++ packages/web-react/src/Components/FisherfolkRecord/FisherfolkArchiveRecord.tsx
@@ -1,11 +1,18 @@
 import React, {useState} from 'react';
 import { Box, Grid, Paper, Typography } from '@mui/material';
 import FisherfolkArchiveTable from '../Table/FisherfolkArchiveTable';
-import { ArchiveFisherfolkDocument, ArchiveFisherfolkQuery } from '../../graphql/generated';
+import {
+  ArchiveFisherfolkDocument, ArchiveFisherfolkQuery,
+  ArchiveGearDocument, ArchiveVesselDocument} from '../../graphql/generated';
 import { useQuery } from '@apollo/client';
+import BasicTabs, { Item } from '../Tab/BasicTab';
+import GearArchiveTable from '../Table/GearArchiveTable';
+import VesselArchiveTable from '../Table/VesselArchiveTable';
 
 const FisherfolkArchiveRecord = () => {
   const { loading, error, data, refetch } = useQuery(ArchiveFisherfolkDocument);
+  const gear = useQuery(ArchiveGearDocument);
+  const vessel = useQuery(ArchiveVesselDocument);
 
   const [fisherfolks, setFisherfolks] = useState<
     ArchiveFisherfolkQuery | undefined
@@ -14,28 +21,18 @@ const FisherfolkArchiveRecord = () => {
   return (
     <Grid container spacing={2}>
       <Grid item xs={12}>
-        <Paper
-          sx={{
-            p: .5,
-            display: 'flex',
-            flexDirection: 'column',
-            boxShadow: 2,
-          }}
-        >
-          <Box m={1}>
-            <Typography variant="h6">Fisherfolk Archive Record</Typography>
-          </Box>
-          <Grid item>
-            <Box m={1}>
-              <FisherfolkArchiveTable
-                error={error}
-                loading={loading}
-                data={fisherfolks ?? data}
-                {...refetch}
-              />
-            </Box>
+        <Item sx={{ p: 0 }}>
+          <Grid container>
+            <BasicTabs
+              tab1Label="Fisherfolks"
+              tab2Label="Gears"
+              tab3Label="Boats"
+              tabelPanel1={<FisherfolkArchiveTable error={error} loading={loading} data={data} />}
+              tabelPanel2={<GearArchiveTable error={gear.error} loading={gear.loading} data={gear.data} />}
+              tablePanel3= {<VesselArchiveTable error={vessel.error} loading={vessel.loading} data={vessel.data} />}
+            />
           </Grid>
-        </Paper>
+        </Item>
       </Grid>
     </Grid>
   );
