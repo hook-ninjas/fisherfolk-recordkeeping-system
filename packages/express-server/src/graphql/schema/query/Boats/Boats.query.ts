4aa1e5ed8cecc9cf8236bc29341c9a82fef87c54
karmslajo <karms.lajo@gmail.com>
2023-04-28 21:08:25 +0800

    add vessel query

--- packages/express-server/src/graphql/schema/query/Boats/Boats.query.ts
+++ packages/express-server/src/graphql/schema/query/Boats/Boats.query.ts
@@ -2,51 +2,74 @@ import { arg, list, nonNull, queryField } from 'nexus';
 
 const Vessels = queryField('vessels', {
   type: nonNull(list(nonNull('Vessel'))),
-  resolve: (_parent, _args, ctx) => ctx.prisma.vessel.findMany({
-    orderBy: [
-      {
-        id: 'desc'
-      }
-    ]
-  })
+  resolve: (_parent, _args, ctx) =>
+    ctx.prisma.vessel.findMany({
+      orderBy: [
+        {
+          id: 'desc',
+        },
+      ],
+    }),
 });
 
 const QueryFisherfolkVessels = queryField('fisherfolkVessels', {
   type: nonNull(list(nonNull('Vessel'))),
   args: {
-    fisherfolkId: nonNull(arg({
-      type: 'BigInt',
-    })),
+    fisherfolkId: nonNull(
+      arg({
+        type: 'BigInt',
+      })
+    ),
   },
-  resolve: (_parent, args, ctx) => ctx.prisma.vessel.findMany({
-    where: {
-      fisherfolkId: args.fisherfolkId
-    }
-  }) 
+  resolve: (_parent, args, ctx) =>
+    ctx.prisma.vessel.findMany({
+      where: {
+        fisherfolkId: args.fisherfolkId,
+      },
+    }),
 });
 
 const QueryAllFisherfolkVessels = queryField('totalFisherfolkVessels', {
   type: 'Int',
   args: {
-    fisherfolkId: nonNull(arg({
-      type: 'BigInt',
-    })),
+    fisherfolkId: nonNull(
+      arg({
+        type: 'BigInt',
+      })
+    ),
   },
-  resolve: (_, args, ctx) => ctx.prisma.vessel.count({
-    where: {
-      fisherfolkId: args.fisherfolkId
-    }
-  })
+  resolve: (_, args, ctx) =>
+    ctx.prisma.vessel.count({
+      where: {
+        fisherfolkId: args.fisherfolkId,
+      },
+    }),
 });
 
 const QueryAllVessels = queryField('totalVessels', {
   type: 'Int',
-  resolve: (_, _args, ctx) => ctx.prisma.vessel.count()
+  resolve: (_, _args, ctx) => ctx.prisma.vessel.count(),
+});
+
+const QueryVessel = queryField('vessel', {
+  type: 'Vessel',
+  args: {
+    id: nonNull(arg({
+      type: 'BigInt'
+    }))
+  },
+  resolve: (_, args, ctx) =>
+    ctx.prisma.vessel.findFirstOrThrow({
+      where: {
+        id: args.id,
+      },
+    }),
 });
 
 export default [
   Vessels,
   QueryFisherfolkVessels,
   QueryAllFisherfolkVessels,
-  QueryAllVessels
+  QueryAllVessels,
+  QueryVessel
 ];
