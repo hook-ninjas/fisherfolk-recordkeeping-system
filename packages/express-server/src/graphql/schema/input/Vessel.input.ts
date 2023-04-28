e792674eb5179007b10669b1af32ad0e732cae20
karmslajo <karms.lajo@gmail.com>
2023-04-28 21:06:49 +0800

    add update vessel input

--- packages/express-server/src/graphql/schema/input/Vessel.input.ts
+++ packages/express-server/src/graphql/schema/input/Vessel.input.ts
@@ -4,14 +4,14 @@ import { Material } from '../enums';
 const CreateVesselInput = inputObjectType({
   name: 'CreateVesselInput',
   definition(t) {
-    t.field('fisherfolkId', {type: 'BigInt'});
+    t.field('fisherfolkId', { type: 'BigInt' });
     t.string('mfvrNumber');
     t.string('homeport');
     t.string('name');
     t.string('type');
     t.string('placeBuilt');
     t.nullable.int('yearBuilt');
-    t.nullable.field('material', {type: Material});
+    t.nullable.field('material', { type: Material });
     t.nullable.float('registeredLength');
     t.nullable.float('registeredBreadth');
     t.nullable.float('registeredDepth');
@@ -26,4 +26,29 @@ const CreateVesselInput = inputObjectType({
   },
 });
 
-export default CreateVesselInput;
+const UpdateVesselInput = inputObjectType({
+  name: 'UpdateVesselInput',
+  definition(t) {
+    t.field('fisherfolkId', { type: 'BigInt' });
+    t.string('mfvrNumber');
+    t.string('homeport');
+    t.string('name');
+    t.string('type');
+    t.string('placeBuilt');
+    t.nullable.int('yearBuilt');
+    t.nullable.field('material', { type: Material });
+    t.nullable.float('registeredLength');
+    t.nullable.float('registeredBreadth');
+    t.nullable.float('registeredDepth');
+    t.nullable.float('tonnageLength');
+    t.nullable.float('tonnageBreadth');
+    t.nullable.float('tonnageDepth');
+    t.nullable.float('grossTonnage');
+    t.nullable.float('netTonnage');
+    t.string('engineMake');
+    t.string('serialNumber');
+    t.nullable.float('horsepower');
+  },
+});
+
+export { CreateVesselInput, UpdateVesselInput };
