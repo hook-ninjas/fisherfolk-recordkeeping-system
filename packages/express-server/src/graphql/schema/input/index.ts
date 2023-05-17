496d439707ac20174f142409c911e81a1ea47f81
DelgadoArlee <delgadoarlee@gmail.com>
2023-05-17 23:11:43 +0800

    Renzo GovernmentAid update

--- packages/express-server/src/graphql/schema/input/index.ts
+++ packages/express-server/src/graphql/schema/input/index.ts
@@ -1,22 +1,10 @@
 import CreateUserInput from './User.input';
-import {
-  CreateFisherfolkInput,
-  UpdateFisherfolkInput,
-} from './Fisherfolk.input';
-import { CreateVesselInput, UpdateVesselInput } from './Vessel.input';
+import { CreateFisherfolkInput, UpdateFisherfolkInput } from './Fisherfolk.input';
+import { CreateVesselInput } from './Vessel.input';
 import CreateGearInput from './Gear.input';
 import CreateImageInput from './Image.input';
-import CreateProgramInput from './GovernmentAid.input';
+import { CreateProgramInput, UpdateProgramInput } from './GovernmentAid.input';
 
-const Inputs = [
-  CreateUserInput,
-  CreateFisherfolkInput,
-  CreateVesselInput,
-  CreateGearInput,
-  CreateImageInput,
-  UpdateFisherfolkInput,
-  CreateProgramInput,
-  UpdateVesselInput
-];
+const Inputs = [CreateUserInput, CreateFisherfolkInput, CreateVesselInput, CreateGearInput, CreateImageInput, UpdateFisherfolkInput, CreateProgramInput, UpdateProgramInput];
 
 export default Inputs;
