cefda1fc5840cc75c7d46135ad8d1e2aa133a3ae
DelgadoArlee <delgadoarlee@gmail.com>
2023-05-22 21:39:08 +0800

    Created fisherfolk livelihood input

--- packages/express-server/src/graphql/schema/input/Livelihood.input.ts
+++ packages/express-server/src/graphql/schema/input/Livelihood.input.ts
@@ -1,13 +1,24 @@
-import { inputObjectType } from 'nexus';
+import { inputObjectType, list } from 'nexus';
 import { SourceOfIncome } from '../enums';
 
-const LivelihoodInput = inputObjectType({
-  name: 'livelihoodInput',
+const CreateLivelihoodInput = inputObjectType({
+  name: 'CreateLivelihoodInput',
   definition(t) {
+    t.nullable.field('fisherfolkId', { type: 'BigInt' });
     t.field('type', { type: SourceOfIncome });
     t.string('description');
     t.boolean('isMain');
   },
 });
 
-export default LivelihoodInput;
+const CreateFfolkLivelihoodInput = inputObjectType({
+  name: 'CreateFfolkLivelihoodInput',
+  definition(t) {
+    t.field('fisherfolkId', { type: 'BigInt' });
+    t.field('mainFishingActivity', { type: SourceOfIncome });
+    t.field('otherFishingActivity', { type: list(SourceOfIncome) });
+    t.string('otherSourceOfIncome');
+  },
+});
+
+export { CreateLivelihoodInput, CreateFfolkLivelihoodInput };
