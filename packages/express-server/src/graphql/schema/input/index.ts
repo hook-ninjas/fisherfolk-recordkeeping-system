e8c59618ff116333d23a9f12bab1b87043a627bb
DelgadoArlee <delgadoarlee@gmail.com>
2023-05-23 09:47:54 +0800

    Updated Organization Input

--- packages/express-server/src/graphql/schema/input/index.ts
+++ packages/express-server/src/graphql/schema/input/index.ts
@@ -2,17 +2,10 @@ import CreateUserInput from './User.input';
 import CreateFisherfolkInput from './Fisherfolk.input';
 import { CreateFfolkVesselInput, CreateVesselInput } from './Vessel.input';
 import CreateGearInput from './Gear.input';
-import { ImageInput, ProfilePhotoInput, UploadImageInput } from './Image.input';
+import { ImageInput, UploadImageInput } from './Image.input';
+import { CreateFfolkLivelihoodInput, CreateLivelihoodInput } from './Livelihood.input';
+import { CreateFfolkOrganizationInput, OrganizationInput } from './Organization.input';
 
-const Inputs = [
-  CreateUserInput,
-  CreateFisherfolkInput,
-  CreateVesselInput,
-  CreateFfolkVesselInput,
-  CreateGearInput,
-  ProfilePhotoInput,
-  UploadImageInput,
-  ImageInput,
-];
+const Inputs = [CreateUserInput, CreateFisherfolkInput, CreateFfolkLivelihoodInput, CreateFfolkOrganizationInput, OrganizationInput, CreateLivelihoodInput, CreateVesselInput, CreateFfolkVesselInput, CreateGearInput, UploadImageInput, ImageInput];
 
 export default Inputs;
